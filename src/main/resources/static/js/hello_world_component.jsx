dfc65fb39b924317f6d0fd50f37c10df52b03c10
Diktas <g.simanelis@gmail.com>
2017-05-15 21:05:35 +0300

    Hello from React !!!

---
+++ src/main/resources/static/js/hello_world_component.jsx
@@ -0,0 +1,10 @@
+
+var HelloWorldComponent = React.createClass({
+  render: function() {
+    return (
+      <div>Hello from React component. Here starts React !</div>
+    );
+  }
+});
+
+window.HelloWorldComponent = HelloWorldComponent;
