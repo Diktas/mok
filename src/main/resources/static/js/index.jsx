2344fa8bcfd85aa6b014a5b0cb7554f19e7ff0fe
Diktas <g.simanelis@gmail.com>
2017-05-16 21:41:32 +0300

    Hello updated. (router)

--- src/main/resources/static/js/index.jsx
+++ src/main/resources/static/js/index.jsx
@@ -1,9 +1,29 @@
-var App = React.createClass({
-  render: function() {
-    return (
-      <HelloWorldComponent />
-    );
-  }
+var App = React.createClass( {
+    render: function() {
+        return (
+            <div style={{ paddingTop: '20px' }}>
+                {this.props.children}
+            </div>
+        );
+    }
 });
 
-ReactDOM.render(<App />, document.getElementById('root'));
+var NoMatch = React.createClass( {
+    render: function() {
+        return <div>Route did not match</div>;
+    }
+});
+
+var Router = ReactRouter.Router;
+var Route = ReactRouter.Route;
+var IndexRoute = ReactRouter.IndexRoute;
+var hashHistory = ReactRouter.hashHistory;
+
+ReactDOM.render((
+    <Router history={hashHistory}>
+        <Route path="/" component={App}>
+            <IndexRoute component={HelloWorldComponent} />
+            <Route path="/hello-world" component={HelloWorldComponent} />
+        </Route>
+    </Router>
+), document.getElementById( 'root' ) );
